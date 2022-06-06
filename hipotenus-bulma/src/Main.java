74f3b243b480c78648f3ef796d7d211b81bcd95a
Hande Nur UYGUN <104138485+handeuygunn@users.noreply.github.com>
2022-06-07 00:27:55 +0300

    Add files via upload

---
+++ hipotenus-bulma/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+         //değişkenleri oluştur
+
+        int a, b ;
+        double c ;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1.kenarı giriniz:");
+        a = input.nextInt();
+        System.out.println("2.kenarı giriniz:");
+        b = input.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+
+
+        System.out.println(c);
+
+
+
+
+    }
+}
